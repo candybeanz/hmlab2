cc9d5185b87a5a41fecf39c16f5059d307be7d7c
Candy <SHWEEAIN.24@ichat.sp.edu.sg>
2025-05-26 01:22:25 +0800

    ex2 commit

--- lab2.py
+++ lab2.py
@@ -1,2 +1,13 @@
 print("ET0735 (Devops for AIOT) -Lab2- Introduction to Python")
-
+def display_m_m():
+    print("display_m_m")
+def user_input():
+    print("user_input")
+def av():
+    print("av")
+def mm():
+    print("mm")
+def sort():
+    print("sort")
+def med():
+    print("med")
